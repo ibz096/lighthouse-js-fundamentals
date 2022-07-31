5dd1efed6926b94789ae1566d3e7b36e7121ce5c
Ibrahim Ahmed <ibrahim.ahmed1996@gmail.com>
2022-07-31 18:35:56 +0000

    Initial Commit

---
+++ tinkering.js
@@ -0,0 +1,10 @@
+function multiply(a, b) {
+    return a * b
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+console.log(multiply(4, 4));
+console.log(multiply(5, 4));
+console.log(multiply(30, 30));
